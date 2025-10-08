bf2bfcf230a94787f974f7c47ea5ddac041bad35
Hasnain Zafar <hasnainpak3@gmail.com>
2025-10-08 17:47:32 +0500

    feat: enhance Career and RecentOpenings components with detailed job descriptions and layout improvements

--- src/shared/components/careerComponents/Career.tsx
+++ src/shared/components/careerComponents/Career.tsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import Image from "next/image";
 const Career = () => {
-  return <div>Career</div>;
+  return (
+    <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row items-center justify-between py-30">
+      <div className="flex flex-col space-y-2 max-w-[550px] ">
+        <p className="text-[#000000B8] font-medium text-[16px] leading-[24px] uppercase">
+          Email Support Services
+        </p>
+        <h2 className="text-black font-sora font-semibold text-[44px] leading-[110%]">
+          We’re always searching <br /> for amazing people <br /> to join our
+          team
+        </h2>
+        <p className="text-[#0000008A] font-normal text-[18px] leading-[27px]">
+          At our company, we believe that amazing people make all the
+          difference. That’s why we’re continuously searching for exceptional
+          individuals to join our team. If you’re driven, talented, and eager to
+          contribute to meaningful work, seize this opportunity.
+        </p>
+      </div>
+      <div className="mt-20 flex justify-center">
+        <Image
+          src="/hero2.png"
+          alt="Career illustration"
+          width={550}
+          height={200}
+          className="object-cover md:translate-y-[36px] shadow-3xl rounded-md"
+          priority
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Career;
--- src/shared/components/careerComponents/RecentOpenings.tsx
+++ src/shared/components/careerComponents/RecentOpenings.tsx
@@ -1,7 +1,74 @@
 import React from "react";
 
 const RecentOpenings = () => {
-  return <div>Recent</div>;
+  const jobs = [
+    {
+      location: "Islamabad",
+      timeAgo: "2 days ago",
+      title: "Taxi Dispatcher",
+      description:
+        "A Taxi Dispatcher assigns drivers and manages bookings to ensure smooth, timely service.",
+      positions: "1 Position",
+    },
+    {
+      location: "Nowshera",
+      timeAgo: "4 days ago",
+      title: "ICabbi Operator",
+      description:
+        "Handle client inquiries efficiently and provide top-quality support for our customers.",
+      positions: "1 Position",
+    },
+  ];
+
+  return (
+    <div className="max-w-[1200px] mx-auto text-center py-20">
+      <p className="text-[#000000B8] font-medium text-[16px] leading-[24px] uppercase">
+        Jobs Position
+      </p>
+      <h2 className="text-black font-sora font-semibold text-[44px] leading-[110%] mt-2 mb-4">
+        Recent Job Openings
+      </h2>
+      <p className="text-[#0000008A] font-normal text-[18px] leading-[27px] max-w-[620px] mx-auto">
+        Our specialized email support delivers fast, accurate, and professional
+        responses, helping you build stronger customer relationships and
+        maintain clear communication.
+      </p>
+      <div className="mt-16 flex w-[820px] gap-8 justify-center">
+        {jobs.map((job, index) => (
+          <div
+            key={index}
+            className="bg-[#FAFAFA] border border-[#E6E6E6] rounded-lg text-left flex flex-col"
+          >
+            {" "}
+            <div className="flex justify-between items-center px-6 pt-5">
+              <p className="text-[#000000B8] font-normal text-[16px] leading-[24px]">
+                {job.timeAgo}
+              </p>
+              <p className="text-[#000000B8] font-normal text-[16px] leading-[24px]">
+                {job.location}
+              </p>
+            </div>
+            <div className="px-6 pt-6 pb-9">
+              <h3 className="text-black font-sora font-semibold text-[28px] leading-[100%] mb-3">
+                {job.title}
+              </h3>
+              <p className="text-[#0000008A] font-normal text-[14px] leading-[120%] max-w-[380px]">
+                {job.description}
+              </p>
+            </div>
+            <div className="bg-[#F0F0F0] flex justify-between items-center px-6 py-4 rounded-b-lg ">
+              <div className="bg-[#FE9C0029] border border-[#FE9C0070] text-[#000000] rounded-[32px] px-4 py-2 text-sm font-medium">
+                {job.positions}
+              </div>
+              <button className="bg-[#FE9C00] text-black text-sm font-medium rounded-[8px] px-4 py-2 hover:opacity-90 transition">
+                Apply
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default RecentOpenings;
